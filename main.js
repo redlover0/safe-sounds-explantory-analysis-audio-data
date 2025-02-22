8deecbf9645ba01cb7bbe782ee295fc4491a2518
Dhamari Trice-Hanson <dhamaritricehanson@gmail.com>
2025-02-22 13:30:58 -0500

    set up

---
+++ main.js
@@ -0,0 +1,11 @@
+const csv = require('csv-parser');
+const fs = require('fs');
+const Chart = require('cli-chart');       
+const prompt = require('prompt');         
+
+
+const SafeSoundMenu = async () => {
+    console.log("_".repeat(50))
+    console.log("\n Welcome to SafeSound, creating advanced audio analytics to meet intellegent amplifcation, setting a new standards on using science of sound")
+    console.log("_".repeat(50))
+};
